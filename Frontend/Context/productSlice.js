7503a1c01da1a8fff469ea5a8810b9d045985e05
itisameerkhan <itisameerkhan@gmail.com>
2024-02-14 09:15:21 +0530

    initialized @redux/toolkit react-redux

---
+++ Frontend/Context/productSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+import all_product from "../Components/assets/all_product";
+
+const productSlice = createSlice({
+    name: 'product',
+    initialState: all_product,
+    reducers: {
+        addProducts: (state, action) => {
+            return action.payload
+        },
+    }
+});
+
+export const { addProducts } = productSlice.actions;
+export default productSlice.reducer;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.scss'
+import './index.scss';
+import appStore from '../Context/appStore.js';
+import { Provider } from 'react-redux';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Provider store={appStore}>
+        <App />
+    </Provider>
+);
